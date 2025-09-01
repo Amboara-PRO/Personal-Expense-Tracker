826aae6fea83d7ca5cac395ea3df056b2b124a9e
Mikaiah <mikaiahranaivoson@gmail.com>
2025-09-01 13:43:47 +0300

    project configuration front/back

---
+++ front/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <p className='text-8xl text-red-50 text-center'>Tailwindcss</p>
+    </>
+  )
+}
+
+export default App
